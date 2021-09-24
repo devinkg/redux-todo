23cd1bf15ab60c1cc913d14c6b27ed6543a5dfec
Devin Godage <devinkanilka@gmail.com>
2021-09-25 05:03:52 +0530

    regular input field added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Input from './components/Input';
 
 function App() {
   return (
     <div className="App">
-      <h1>DeeKay is here</h1>
+    {/* todolist */}
+    {/* done */}
+    <Input/>
     </div>
   );
 }
