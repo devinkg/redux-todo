d2480c269e3ec379139bf4ec1424efb9327dfce4
Devin Godage <devinkanilka@gmail.com>
2021-09-28 02:39:05 +0530

    Redux applied

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,16 +1,26 @@
 import React, { useState } from 'react';
 import './Input.css';
 
+import { useDispatch } from 'react-redux';
+import { saveTodo } from '../features/todoSlice';
+
 const Input = () => {
     const [input, setInput] = useState('');
-    
-    const addTodo = () => {
+    const dispatch = useDispatch();
 
+    const addTodo = () => {
+        console.log(`Adding ${input}`)
+
+        dispatch(saveTodo({
+            item: input,
+            done: false,
+            id: Date.now()
+        }))
     }
 
     return (
         <div class='input'>
-            <input type="text" value={input} onChange={e=>setInput(e.target.value)}/>
+            <input type="text" value={input} onChange={e => setInput(e.target.value)} />
             <button onClick={addTodo}>Add!</button>
         </div>
     )
