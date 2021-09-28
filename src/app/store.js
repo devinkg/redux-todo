90a062513072d4f47542961af3c26699a70d10da
Devin Godage <devinkanilka@gmail.com>
2021-09-29 02:51:02 +0530

    Essential comment added

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import todoReducer from '../features/todoSlice';
 
+//Configuring store
 export const store = configureStore({
   reducer: {
     todos: todoReducer
