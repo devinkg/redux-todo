6de9e1a1ea9468c647d7ea00726434455a2910db
Devin Godage <devinkanilka@gmail.com>
2021-09-30 02:17:59 +0530

    TodoItem added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import './App.css';
 import Input from './components/Input';
+import TodoItem from './components/TodoItem';
+
+const todoList = [
+  {
+    item: 'todo',
+    done: false,
+    id: 65548448787
+  },
+  {
+    item: 'todo2',
+    done: true,
+    id: 65548448783
+  }
+]
 
 function App() {
   return (
     <div className="app">
-    {/* todolist */}
-    {/* done */}
-    <Input/>
+      <div className="app_container">
+        <div className="app_todoContainer">
+          {todoList.map((item) => (
+            <TodoItem />
+          ))}
+        </div>
+        <Input />
+      </div>
     </div>
   );
 }
---
+++ src/components/TodoItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TodoItem = () => {
+    return (
+        <div className='todoItem'>
+
+        </div>
+    )
+}
+
+export default TodoItem;
